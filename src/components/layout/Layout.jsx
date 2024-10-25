f0a9349b68383e6b842f02f393057f5d7d45c9c2
Ammad Ahmed <63580683+ammad-ahmed1@users.noreply.github.com>
2024-10-26 01:34:56 +0500

    mobil-navbar fix

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -6,7 +6,11 @@ const Layout = ({ children }) => {
   return (
     <>
       <div className="flex flex-col min-h-screen min-w-screen text-textColor">
-        <Navbar />
+        <div className="sticky top-0 z-30 bg-white">
+          {" "}
+          <Navbar />
+        </div>
+
         <main className="flex-grow py-4">{children}</main>
         <div className="w-90p mx-auto px-6">
           <Footer />
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -15,75 +15,83 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="text-black py-4 px-0 z-20">
-      <div className="flex items-center justify-between w-90p m-auto px-5">
-        <div className="flex items-center">
-          <img
-            src="https://webmomentum.co.uk/digital-marketing-reading-img/2024/02/cropped-web-momentum.png"
-            alt="Web Momentum Logo"
-            className="h-14 py-2"
-          />
-          <h3 className="hidden lg:flex items-center text-[28px] font-bold py-2 px-2 font-serif">
-            Web Momentum
-          </h3>
-        </div>
+    <div className="relative">
+      <nav className="text-black py-4 px-0 z-20 sticky">
+        <div className="flex items-center justify-between w-90p m-auto px-5">
+          <div className="flex items-center sticky">
+            <img
+              src="https://webmomentum.co.uk/digital-marketing-reading-img/2024/02/cropped-web-momentum.png"
+              alt="Web Momentum Logo"
+              className="h-14 py-2"
+            />
+            <h3 className="hidden lg:flex items-center text-[28px] font-bold py-2 px-2 font-serif">
+              Web Momentum
+            </h3>
+          </div>
 
-        <div className="hidden lg:flex space-x-4 items-center">
-          {navItems.map(({ name, href }) => (
-            <a
-              key={name}
-              href={href}
-              className="hover:text-gray-400 py-0 px-8 font-semibold text-[20px] font-sans"
+          <div className="hidden lg:flex space-x-4 items-center">
+            {navItems.map(({ name, href }) => (
+              <a
+                href={href}
+                className="block py-0 px-8 font-semibold text-[20px] font-sans hover:text-gray-400"
+              >
+                {name}
+              </a>
+            ))}
+          </div>
+
+          <div className="block lg:hidden">
+            <button
+              className="menu-toggle focus:outline-none"
+              onClick={toggleMenu}
+              aria-label="Toggle menu"
             >
-              {name}
-            </a>
-          ))}
-        </div>
-
-        <div className="block lg:hidden">
-          <button
-            className="menu-toggle focus:outline-none"
-            onClick={toggleMenu}
-            aria-label="Toggle menu"
-          >
-            <svg
-              viewBox="0 0 512 512"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="1em"
-              height="1em"
-            >
-              <path d="M0 96c0-13.255 10.745-24 24-24h464c13.255 0 24 10.745 24 24s-10.745 24-24 24H24c-13.255 0-24-10.745-24-24zm0 160c0-13.255 10.745-24 24-24h464c13.255 0 24 10.745 24 24s-10.745 24-24 24H24c-13.255 0-24-10.745-24-24zm0 160c0-13.255 10.745-24 24-24h464c13.255 0 24 10.745 24 24s-10.745 24-24 24H24c-13.255 0-24-10.745-24-24z"></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-
-      {isOpen && (
-        <div className="fixed inset-0 bg-white z-50 flex flex-col justify-start items-center">
-          <button
-            className="close-button text-black m-4 self-end"
-            onClick={toggleMenu}
-          >
-            Close
-          </button>
-          <div className="flex-grow flex justify-center items-start py-4">
-            <ul className="flex flex-col space-y-4 text-center my-4">
-              {navItems.map(({ name, href }) => (
-                <li key={name}>
-                  <a
-                    href={href}
-                    className="py-2 text-lg hover:text-gray-400 font-serif text-[22px]"
-                  >
-                    {name}
-                  </a>
-                </li>
-              ))}
-            </ul>
+              <svg
+                viewBox="0 0 512 512"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+              >
+                <path d="M0 96c0-13.255 10.745-24 24-24h464c13.255 0 24 10.745 24 24s-10.745 24-24 24H24c-13.255 0-24-10.745-24-24zm0 160c0-13.255 10.745-24 24-24h464c13.255 0 24 10.745 24 24s-10.745 24-24 24H24c-13.255 0-24-10.745-24-24zm0 160c0-13.255 10.745-24 24-24h464c13.255 0 24 10.745 24 24s-10.745 24-24 24H24c-13.255 0-24-10.745-24-24z"></path>
+              </svg>
+            </button>
           </div>
         </div>
-      )}
-    </nav>
+
+        {/* {isOpen && ( */}
+        <div
+          className={`transition-opacity duration-200 ${
+            isOpen ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          >
+          <div className="fixed inset-0 bg-white z-50 flex flex-col justify-start items-center ">
+            <button
+              className="close-button text-black m-4 self-end"
+              onClick={toggleMenu}
+            >
+              Close
+            </button>
+            <div className="flex-grow flex justify-center items-start py-4">
+              <ul className="flex flex-col space-y-4 text-center my-4">
+                {navItems.map(({ name, href }) => (
+                  <li key={name}>
+                    <a
+                      href={href}
+                      className="py-2 text-lg hover:text-gray-400 font-serif text-[22px]"
+                    >
+                      {name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        {/* )} */}
+      </nav>
+    </div>
   );
 };
 
